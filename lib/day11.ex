bbf26734b256a8d462cdd047ee2ea48968e69c81
Matthew O'Neill <oneill_91@hotmail.co.uk>
2023-12-11 07:58:54 +0000

    refactor day11

--- lib/day11.ex
+++ lib/day11.ex
@@ -1,31 +1,8 @@
 defmodule Day11 do
-  def parse_grid(lines) do
-    lines
-    |> String.split()
-    |> Enum.map(&String.graphemes/1)
-    |> parse_rows([])
-    |> rotate
-    |> parse_rows([])
-    |> rotate
-  end
-
   def rotate(list) do
     list
     |> Enum.zip()
     |> Enum.map(&Tuple.to_list/1)
-  end
-
-  def parse_rows([], rows), do: rows |> Enum.reverse()
-
-  def parse_rows([curr | next], rows) do
-    res =
-      if Enum.all?(curr, &(&1 == ".")) do
-        [curr | [curr | rows]]
-      else
-        [curr | rows]
-      end
-
-    parse_rows(next, res)
   end
 
   def expand_universe(rows, jump) do
@@ -39,11 +16,11 @@ defmodule Day11 do
     end)
     |> Enum.reduce({0, []}, fn {row, current}, {previous, list} ->
       new_idx = previous + current
-      {new_idx, list ++ [{row, new_idx - 1}]}
+      {new_idx, list ++ [{row, new_idx}]}
     end)
     |> elem(1)
-    |> Enum.reduce({[], []}, fn {row, idx}, {rows, ys} ->
-      {rows ++ [row], ys ++ [idx]}
+    |> Enum.reduce({[], []}, fn {row, idx}, {rows, idxs} ->
+      {rows ++ [row], idxs ++ [idx]}
     end)
   end
 
@@ -54,10 +31,7 @@ defmodule Day11 do
       |> Enum.map(&String.graphemes/1)
       |> expand_universe(jump)
 
-    {_columns, xs} =
-      rows
-      |> rotate()
-      |> expand_universe(jump)
+    xs = rows |> rotate() |> expand_universe(jump) |> elem(1)
 
     for {row, y} <- Enum.zip(rows, ys) do
       for {char, x} <- Enum.zip(row, xs), char == "#" do
@@ -69,42 +43,15 @@ defmodule Day11 do
 
   def main() do
     grid = File.read!("inputs/day11.txt")
-
-    grid |> parse_grid |> part1 |> IO.inspect(label: "part 1")
-
-    grid |> parse_massive(1_000_000) |> part2 |> IO.inspect(label: "part 2")
+    grid |> parse_massive(2) |> parts |> IO.inspect(label: "part 1")
+    grid |> parse_massive(1_000_000) |> parts |> IO.inspect(label: "part 2")
   end
 
   def manhattan({y1, x1}, {y2, x2}) do
     abs(x1 - x2) + abs(y1 - y2)
   end
 
-  def part1(grid) do
-    galaxies =
-      grid
-      |> Enum.with_index()
-      |> Enum.reduce(
-        [],
-        fn {row, y}, acc ->
-          [
-            row
-            |> Enum.with_index()
-            |> Enum.filter(fn
-              {".", _} -> false
-              {"#", _} -> true
-            end)
-            |> Enum.map(fn {_item, x} -> {y, x} end)
-            | acc
-          ]
-        end
-      )
-      |> List.flatten()
-
-    galaxies
-    |> part2
-  end
-
-  def part2(galaxies) do
+  def parts(galaxies) do
     galaxies
     |> Enum.reduce({0, galaxies}, fn
       current, {total, [_ | remaining]} ->
