e3981eda2ca61fffe206a6874a5d080c86be2fb3
Matthew O'Neill <oneill_91@hotmail.co.uk>
2023-12-14 21:03:07 +0000

    part 2 implementation

--- lib/day14.ex
+++ lib/day14.ex
@@ -1,54 +1,100 @@
 defmodule Day14 do
-  # defstruct
-
   def parse(lines) do
-    lines
-    |> String.split()
-    |> Enum.map(fn row -> String.graphemes(row) end)
+    lines |> String.split() |> Enum.map(fn row -> String.graphemes(row) end)
   end
 
   def main() do
     grid = File.read!("inputs/day14.txt") |> parse
-
     grid |> part1 |> IO.inspect(label: "part 1")
-  end
-
-  def transpose(rows) do
-    rows |>  Enum.zip() |> Enum.map(&Tuple.to_list/1)
+    grid |> part2 |> IO.inspect(label: "part 2")
   end
 
   def part1(grid) do
-    grid |> transpose
-    |> Enum.map( &sort(&1))
-    |> Enum.map( fn column -> size = length(column)
-        column |> Enum.with_index |> Enum.map(
-          fn {"O", idx} -> idx + size
-          _ -> 0
-          end
-        )
-        |> Enum.sum
-    end)
-    |> Enum.sum
+    grid |> transpose |> Enum.map(&sort/1) |> calculate_load
   end
 
-  def part2(_data) do
+  def part2(grid, cycles \\ 1_000_000) do
+    Stream.cycle([0])
+    |> Enum.reduce_while({0, grid, %{}}, fn _, {count, state, seen} ->
+      new_state = cycle(state)
+
+      if length(Map.get(seen, new_state, [])) > 4 do
+        {:halt, seen}
+      else
+        {:cont, {count + 1, new_state, Map.update(seen, new_state, [count], &[count | &1])}}
+      end
+    end)
+    |> Stream.filter(fn {_state, seen} -> length(seen) > 1 end)
+    |> Stream.map(fn {state, seen} -> {state, hd(seen), differences(seen)} end)
+    |> Stream.map(fn {state, head, [diff | _]} ->
+      if is_cycle_state?(cycles - 1, head, diff), do: state
+    end)
+    |> Enum.filter(& &1)
+    |> hd
+    |> transpose
+    |> calculate_load
+  end
+
+  def differences(seen) do
+    Enum.chunk_every(seen, 2, 1, :discard)
+    |> Enum.map(fn [first, second] -> first - second end)
+  end
+
+  def is_cycle_state?(cycles, head, diff),
+    do: (cycles - head) / diff == div(cycles - head, diff)
+
+  def calculate_load(grid) do
+    grid
+    |> Enum.map(fn row ->
+      len = length(row)
+
+      row
+      |> Stream.with_index()
+      |> Enum.map(fn
+        {"O", idx} -> len - idx
+        {_item, _idx} -> 0
+      end)
+    end)
+    |> List.flatten()
+    |> Enum.sum()
+  end
+
+  def transpose(rows) do
+    rows |> Stream.zip() |> Stream.map(&Tuple.to_list/1)
+  end
+
+  def cycle(grid) do
+    grid
+    # north
+    |> transpose()
+    |> Stream.map(&sort/1)
+    # west
+    |> transpose()
+    |> Stream.map(&sort/1)
+    # south
+    |> transpose()
+    |> Stream.map(&(sort(Enum.reverse(&1)) |> Enum.reverse()))
+    # east
+    |> transpose
+    |> Enum.map(&(sort(Enum.reverse(&1)) |> Enum.reverse()))
   end
 
   def sort(current), do: sort(current, [])
   def sort(current, current), do: current
-  def sort(current, previous) do
+
+  def sort(current, _) do
     sort(sort_boulder(current, []), current)
   end
 
   def sort_boulder(current, previous)
 
   def sort_boulder([], previous), do: Enum.reverse(previous)
+
   def sort_boulder([".", "O" | rest], previous) do
     sort_boulder(["." | rest], ["O" | previous])
   end
+
   def sort_boulder([head | rest], previous) do
     sort_boulder(rest, [head | previous])
   end
-
-
 end
--- test/day14_test.exs
+++ test/day14_test.exs
@@ -22,14 +22,70 @@ O..#.OO...
 ..O..#.O.O
 ..O.......
 #....###..
-#....#...." |> String.split |> Enum.map(&String.graphemes/1) |> transpose
+#....#...." |> String.split() |> Enum.map(&String.graphemes/1)
+
+  @cycle1 ".....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#...." |> String.split() |> Enum.map(&String.graphemes/1)
+
+  @cycle2 ".....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#..OO###..
+#.OOO#...O" |> String.split() |> Enum.map(&String.graphemes/1)
+
+  @cycle3 ".....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#...O###.O
+#.OOO#...O" |> String.split() |> Enum.map(&String.graphemes/1)
 
   test "number of items in example map == 35" do
     assert @example |> parse |> part1 == 136
   end
 
   test "sorted example == result" do
-    assert @example |> parse |> transpose |> sort == @result
+    assert @example |> parse |> transpose |> Enum.map(&sort/1) == @result |> transpose |> Enum.to_list
   end
 
+  test "sort_boulder" do
+    assert ~w[. O] |> sort_boulder([]) == ~w[O .]
+    assert ~w[# O] |> sort_boulder([]) == ~w[# O]
+    assert ~w[. O . O] |> sort_boulder([]) == ~w[O . O .]
+    assert ~w[. O # O] |> sort_boulder([]) == ~w[O . # O]
+  end
+
+  test "sort" do
+    assert ~w[. O] |> sort() == ~w[O .]
+    assert ~w[# O] |> sort() == ~w[# O]
+    assert ~w[. O . O] |> sort() == ~w[O O . .]
+    assert ~w[. O # O . . O . O] |> sort([]) == ~w[O . # O O O . . .]
+  end
+
+  test "cycle" do
+    assert @example |> parse |> cycle == @cycle1
+    assert @example |> parse |> cycle |> cycle == @cycle2
+    assert @example |> parse |> cycle |> cycle |> cycle == @cycle3
+  end
+
+  test "part2 == 64" do
+    assert @example |> parse |> part2 == 63
+  end
 end
